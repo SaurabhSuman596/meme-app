67116a48248bcb3d0974c28342fef5f332d8571b
Saurabh-Suman596 <saurabhsuman819@gmail.com>
2022-06-24 10:55:19 +0530

    back to world

--- src/apicall.js
+++ src/apicall.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const GetAllPost = async function (setMeme) {
   let res = await axios.get(
-    'https://meme-api.herokuapp.com/gimme/IndianDankMemes/50'
+    'https://meme-api.herokuapp.com/gimme/50'
   );
   return setMeme(res.data.memes);
 };
